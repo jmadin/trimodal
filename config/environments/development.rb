dc002741234138aff533dcfe2d0871bd32c05961
Joshua Madin <joshua.madin@mq.edu.au>
2013-09-18 07:36:06 +1000

    Some changes.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Trimodal::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
   
   config.action_dispatch.default_headers = {
       'X-Frame-Options' => 'ALLOWALL'
