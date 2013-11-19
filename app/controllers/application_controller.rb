887c0131e212357cbdf32463b545d278a6847a36
Joshua Madin <joshua.madin@mq.edu.au>
2013-11-19 15:03:43 +1000

    Changes to export function for corals and areas.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,9 +19,13 @@ class ApplicationController < ActionController::Base
   def export_tagged  
     @all = Observation.find_by_sql("SELECT *, observations.id as obs_id, corals.notes as coral_notes, observations.notes as observation_notes, areas.notes as area_notes FROM observations, corals LEFT OUTER JOIN areas ON observations.id = areas.observation_id WHERE corals.id = observations.coral_id")  
     csv_string = CSV.generate do |csv|   
-      csv << ["colony_id", "transect", "map_x", "map_y", "dim_1", "dim_2", "species", "species_code", "colony_notes", "observation_id", "fieldtrip_id", "mother_id", "image_tag", "active_tag", "action", "observation_notes", "area_id", "photographer_name", "outliner_name", "photo_acceptable", "outline_acceptable", "outline_filename", "area_cm2", "marked", "area_notes"]      
+      csv << ["colony_id", "transect", "map_x", "map_y", "species", "species_code", "observation_id", "fieldtrip_id", "mother_id", "image_tag", "active_tag", "action", "area_id", "photographer_name", "outliner_name", "photo_acceptable", "outline_acceptable", "outline_filename", "area_cm2", "marked", "colony_notes", "observation_notes", "area_notes"]      
       @all.each do |cor|
-        csv << [cor.coral_id, cor.transect, cor.map_x, cor.map_y, cor.dim_1, cor.dim_2, cor.species, cor.species_code, cor.coral_notes, cor.obs_id, "F#{cor.fieldtrip_id}", cor.mother_id, cor.tag_number, cor.active_tag, cor.action, cor.observation_notes, cor.id, cor.photographer_name, cor.outliner_name, cor.acceptable, cor.acceptable_outline , cor.outline_filename, cor.area, cor.flag, cor.area_notes]
+        fid = cor.fieldtrip_id
+        if fid > 5
+          fid = fid - 5
+        end
+        csv << [cor.coral_id, cor.transect, cor.map_x, cor.map_y, cor.species, cor.species_code, cor.obs_id, "F#{fid}", cor.mother_id, cor.tag_number, cor.active_tag, cor.action, cor.id, cor.photographer_name, cor.outliner_name, cor.acceptable, cor.acceptable_outline , cor.outline_filename, cor.area, cor.flag, cor.coral_notes, cor.observation_notes, cor.area_notes]
         end 
     end 
     send_data csv_string, 
@@ -34,9 +38,13 @@ class ApplicationController < ActionController::Base
   def export_corals
     @all = Observation.find_by_sql("SELECT *, observations.id as obs_id, corals.notes as coral_notes, observations.notes as observation_notes FROM observations, corals WHERE corals.id = observations.coral_id")  
     csv_string = CSV.generate do |csv|   
-      csv << ["colony_id", "transect", "map_x", "map_y", "dim_1", "dim_2", "species", "species_code", "colony_notes", "observation_id", "fieldtrip_id", "mother_id", "tag_number", "active_tag", "action", "observation_notes"]      
+      csv << ["colony_id", "transect", "map_x", "map_y", "species", "species_code", "observation_id", "fieldtrip_id", "mother_id", "tag_number", "active_tag", "action", "colony_notes", "observation_notes"]      
       @all.each do |cor|
-        csv << [cor.coral_id, cor.transect, cor.map_x, cor.map_y, cor.dim_1, cor.dim_2, cor.species, cor.species_code, cor.coral_notes, cor.obs_id, "F#{cor.fieldtrip_id}", cor.mother_id, cor.tag_number, cor.active_tag, cor.action, cor.observation_notes]
+        fid = cor.fieldtrip_id
+        if fid > 5
+          fid = fid - 5
+        end
+        csv << [cor.coral_id, cor.transect, cor.map_x, cor.map_y, cor.species, cor.species_code, cor.obs_id, "F#{fid}", cor.mother_id, cor.tag_number, cor.active_tag, cor.action, cor.coral_notes, cor.observation_notes]
         end 
     end 
     send_data csv_string, 
